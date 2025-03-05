d1c258153c6c32885d3a567e4fe93fb96a9c6958
Michał Jopa <micha.jopa@gmail.com>
2025-03-05 10:46:26 +0100

    added login and logout using session

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,29 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
-    //   const isLoggedIn = req
-    //     .get('Cookie')
-    //     .split(';')[1]
-    //     .trim()
-    //     .split('=')[1] === 'true';
-    console.log(req.session.isLoggedIn);
-    res.render('auth/login', {
-      path: '/login',
-      pageTitle: 'Login',
-      isAuthenticated: false
-    });
-  };
-  
-  exports.postLogin = (req, res, next) => {
-    req.session.isLoggedIn = true;
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: false
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  User.findById('67c1b0fede6aa43822d5c204')
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save(err => {
+        console.log(err);
+        res.redirect('/');
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
     res.redirect('/');
-  };
+  });
+};
